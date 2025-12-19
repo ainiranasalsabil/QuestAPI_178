031701ce27c005906a0f25767ac81148332372c2
ainiranasalsabil <ainiranasalsabil04@gmail.com>
2025-12-19 16:31:34 +0700

    Menambahkan interface ContainerApp untuk manajemen dependensi aplikasi

--- app/src/main/java/com/example/questapi/repositori/ContainerApp.kt
+++ app/src/main/java/com/example/questapi/repositori/ContainerApp.kt
@@ -1,2 +1,15 @@
 package com.example.questapi.repositori
 
+import android.app.Application
+import com.example.questapi.apiservice.ServiceApiSiswa
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+
+interface ContainerApp {
+    val repositoryDataSiswa: RepositoryDataSiswa
+}
+
