4ae691b26d5539300d0e45ccf063d5f05ebf5370
ainiranasalsabil <ainiranasalsabil04@gmail.com>
2025-12-19 17:01:34 +0700

    Menambahkan StatusUiSiswa untuk menangani status UI data siswa

--- app/src/main/java/com/example/questapi/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/example/questapi/viewmodel/HomeViewModel.kt
@@ -1,2 +1,20 @@
 package com.example.questapi.viewmodel
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.questapi.modeldata.DataSiswa
+import com.example.questapi.repositori.RepositoryDataSiswa
+import kotlinx.coroutines.launch
+import retrofit2.HttpException
+import java.io.IOException
+
+sealed interface StatusUiSiswa {
+    data class Success(val listSiswa: List<DataSiswa>) : StatusUiSiswa
+    object Error : StatusUiSiswa
+    object Loading : StatusUiSiswa
+}
+
+
