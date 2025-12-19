2e0a03f2f6c259472c0cc79cad648256a9ec7e2a
ainiranasalsabil <ainiranasalsabil04@gmail.com>
2025-12-19 10:33:58 +0700

    Menambahkan class JaringanRepositorySiswa

--- app/src/main/java/com/example/questapi/repositori/RepositoryDataSiswa.kt
+++ app/src/main/java/com/example/questapi/repositori/RepositoryDataSiswa.kt
@@ -6,3 +6,6 @@ interface RepositoryDataSiswa{
     suspend fun getDataSiswa() : List<DataSiswa>
     suspend fun getDataSiswa(dataSiswa: DataSiswa) :retrofit2.Response<Void>
 }
+class JaringanRepositorySiswa (
+    private val serviceApiSiswa: ServiceApiSiswa
+)
