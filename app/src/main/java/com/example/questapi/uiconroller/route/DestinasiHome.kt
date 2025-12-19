da802034cf83bcc681194658c9b7ac70267e0279
ainiranasalsabil <ainiranasalsabil04@gmail.com>
2025-12-19 18:05:38 +0700

    Menambahkan DestinasiHome sebagai rute halaman utama aplikasi

--- app/src/main/java/com/example/questapi/uiconroller/route/DestinasiHome.kt
+++ app/src/main/java/com/example/questapi/uiconroller/route/DestinasiHome.kt
@@ -1,4 +1,8 @@
 package com.example.questapi.uiconroller.route
 
-object DestinasiHome {
+import com.example.questapi.R
+
+object DestinasiHome: DestinasiNavigasi {
+    override val route = "home"
+    override val titleRes = R.string.app_name
 }
