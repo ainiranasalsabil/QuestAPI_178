6762105316078f9f1e2bc749e8dbbcbc533e73aa
ainiranasalsabil <ainiranasalsabil04@gmail.com>
2025-12-26 09:10:05 +0700

    Menampilkan konten utama, termasuk logika hapus data dan pengaturan scroll

--- app/src/main/java/com/example/questapi/view/HalamanDetail.kt
+++ app/src/main/java/com/example/questapi/view/HalamanDetail.kt
@@ -1,17 +1,24 @@
 package com.example.questapi.view
 
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.questapi.R
 import com.example.questapi.uiconroller.route.DestinasiDetail
 import com.example.questapi.viewmodel.provide.PenyediaViewModel
+import kotlinx.coroutines.launch
+import java.util.function.IntConsumer
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -43,8 +50,24 @@ fun DetailSiswaScreen(
                 )
             ){
                 Icon(
-                    imageVector = Icons.Default.Edit,
+                    imageVector = icons.Default.Edit,
                     contentDescription = stringResource(R.string.update)
                 )
             }
-        },
+        }, modifier = modifier
+    ) { innerPadding ->
+        val coroutineScope = rememberCoroutineScope()
+        BodyDetailDataSiswa(
+            statusUiDetail = viewModel.statusUiDetail,
+            onDelete = {
+                coroutineScope.launch {
+                    viewModel.hapusSatuSiswa()
+                    navigateBack()
+                }
+            },
+            modifier = Modifier
+                .padding(innerPadding)
+                .verticalScroll(rememberScrollState())
+        )
+    }
+}
