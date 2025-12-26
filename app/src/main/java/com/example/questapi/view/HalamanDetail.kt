6c0013f3d1faa90b8b9b15d29a65fba3315b83a6
ainiranasalsabil <ainiranasalsabil04@gmail.com>
2025-12-26 09:16:30 +0700

    Tombol untuk memicu munculnya dialog konfirmasi penghapusan data

--- app/src/main/java/com/example/questapi/view/HalamanDetail.kt
+++ app/src/main/java/com/example/questapi/view/HalamanDetail.kt
@@ -10,7 +10,9 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.rememberCoroutineScope
@@ -103,4 +105,10 @@ private fun BodyDetailDataSiswa(
                 )
             }
             else -> {}
-        }
+        }  OutlinedButton(
+                onClick = { deleteConfirmationRequired = true },
+        shape = MaterialTheme.shapes.small,
+        modifier = Modifier.fillMaxWidth()
+        ) {
+        Text(text = stringResource(R.string.delete))
+    }
